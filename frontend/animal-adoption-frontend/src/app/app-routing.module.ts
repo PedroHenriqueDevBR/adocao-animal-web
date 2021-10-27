62e2f0ec2c2b6f5a1d754705cabcd3628032233d
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-10-26 22:35:54 -0300

    feat: show map with animals

--- frontend/animal-adoption-frontend/src/app/app-routing.module.ts
+++ frontend/animal-adoption-frontend/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ const routes: Routes = [
   { path: 'account', loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: '**', pathMatch: 'full', component: PageNotFoundComponent },
+  { path: '404', pathMatch: 'full', component: PageNotFoundComponent },
 ];
 
 @NgModule({
--- frontend/animal-adoption-frontend/src/app/main/pages/show-animal/show-animal.component.ts
+++ frontend/animal-adoption-frontend/src/app/main/pages/show-animal/show-animal.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
 import { AnimalModel } from 'src/app/shared/models/animal-model';
+import { AnimalService } from 'src/app/shared/services/animal.service';
 
 @Component({
   templateUrl: './show-animal.component.html',
@@ -9,13 +12,80 @@ import { AnimalModel } from 'src/app/shared/models/animal-model';
 export class ShowAnimalComponent implements OnInit {
 
   @Input() animal: AnimalModel | undefined;
+  animalShow: AnimalModel = new AnimalModel();
+  modalRef?: BsModalRef;
+  selectedPhoto: string = '';
 
   constructor(
-    private route: ActivatedRoute
-  ) { }
-
+    private route: ActivatedRoute,
+    private routerService: Router,
+    private animalService: AnimalService,
+    private toast: ToastrService,
+    private modalService: BsModalService,
+  ) {
+    console.log('constructor');
+  }
+  
   ngOnInit(): void {
-    console.log(this.route.snapshot.paramMap.get('id'));
+    if (this.animal == undefined) {
+      const id = this.route.snapshot.paramMap.get('id');
+      if (id != null && isNaN(Number.parseInt(id)) == false) {
+        const animalId = Number.parseInt(id);
+        this.getAnimalFromDatabase(animalId);
+      } else {
+        this.notFoundRedirect();
+      }
+    } else {
+      this.animalShow = this.animal;
+    }
+  }
+
+  getAnimalFromDatabase(id: number) {
+    console.log('getAnimalFromDatabase');
+    this.animalService.getAimalsByID(id).subscribe(
+      (data: AnimalModel) => {
+        console.log(data);
+        this.animalShow = data;
+      },
+      error => this.verifyStatusError(error),
+    );
+  }
+
+  notFoundRedirect() {
+    this.routerService.navigateByUrl('/404');
+  }
+
+  serverPhoto(photo: string): string {
+    return `/server${photo}`;
+  }
+
+  showAnimal(photo: string, template: TemplateRef<any>) {
+    this.selectedPhoto = this.serverPhoto(photo);
+    this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
+  }
+
+  decline(): void {
+    this.modalRef?.hide();
+  }
+
+  verifyStatusError(errors: any) {
+    console.error(errors);
+    if (errors.status >= 500) {
+      this.toast.error('Servidor indisponível');
+    } else if (errors.status == 404) {
+      this.routerService.navigateByUrl('/404');
+    } else if (errors.status == 406) {
+      if (errors.error.errors) {
+        for (let error of errors.error.errors) {
+          this.toast.error(error);
+        }
+      }
+    } else if (errors.status == 403) {
+      this.toast.error('Sem permissão');
+    } else {
+      this.toast.error('Erro interno');
+      console.log(errors);
+    }
   }
 
 }
