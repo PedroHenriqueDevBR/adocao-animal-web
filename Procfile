3ff0a343778cc52f2253cd2627aac4e13ec05e11
PedroHenriqueDevBR <p.lyma18@gmail.com>
2021-11-22 08:29:01 -0300

    chore: configure deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn backend/animal_adoption/animal_adoption.wsgi
--- backend/animal_adoption/animal_adoption/settings.py
+++ backend/animal_adoption/animal_adoption/settings.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+django_heroku.settings(locals())
