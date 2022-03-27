07029a0c5e6a0859d95b2f29835950ea69a1785d
pedrohenriquedevbr <p.lyma18@gmail.com>
2022-03-26 23:29:48 -0300

    chore: initial docker configuration

--- backend/animal_adoption/apps/core/admin.py
+++ backend/animal_adoption/apps/core/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Person, State, City
+from .models import Person, State, City, AnimalType, Animal, AnimalPhoto, VaccineBook, BlockedReason, AdoptionRequest
 
-# Register your models here.
 admin.site.register(Person)
 admin.site.register(State)
 admin.site.register(City)
+admin.site.register(AnimalType)
+admin.site.register(Animal)
+admin.site.register(AnimalPhoto)
+admin.site.register(VaccineBook)
+admin.site.register(BlockedReason)
+admin.site.register(AdoptionRequest)
