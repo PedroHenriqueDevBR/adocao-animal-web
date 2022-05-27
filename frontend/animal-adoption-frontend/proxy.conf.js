aef43782740ee69c507770e2076bba0fc255c9e5
pedrohenriquedevbr <p.lyma18@gmail.com>
2022-05-26 22:21:04 -0300

    feat: apply docker in frontend

--- frontend/animal-adoption-frontend/proxy.conf.js
+++ frontend/animal-adoption-frontend/proxy.conf.js
@@ -1,7 +1,7 @@
 const PROXY_CONFIG = [
     {
         context: ['/server'],
-        target: 'http://localhost:8000',
+        target: 'http://0.0.0.0:8000',
         secure: false,
         // changeOrigin: true,
         logLevel: 'debug',
